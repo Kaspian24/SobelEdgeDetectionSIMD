31c4bf69a645c87082968c1bcf1d04fadbb2f21c
Marcin <92104922+Kaspian24@users.noreply.github.com>
2024-01-06 16:57:21 +0100

    ASM szybszy niż C++, na razie brak komentarzy bo nie ma czasu xD

--- JAAsm/JAAsm.asm
+++ JAAsm/JAAsm.asm
@@ -1,9 +1,12 @@
 .data
 
 align 16
-MatrixX DB 1, 0, -1, 2, -2, 1, 0, -1, 0, 0, 0, 0, 0, 0, 0, 0
-MatrixY DB -1, -2, -1, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0
-ShuffleMatrixA DB 0, 1, 2, 4, 6, 9, 10, 11, 0, 0, 0, 0, 0, 0, 0, 0
+MatrixXandY DB 1, 0, -1, 2, -2, 1, 0, -1, -1, -2, -1, 0, 0, 1, 2, 1
+ShuffleMatrixA DB 0, 1, 2, 4, 6, 9, 10, 11, 0, 1, 2, 4, 6, 9, 10, 11
+ShuffleRGB DB 0, 1, 2, 0, 3, 4, 5, 0, 6, 7, 8, 0, 9, 10, 11, 0
+MaskRGB DB 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0
+DivideByThree DD 3.0, 3.0, 3.0, 3.0
+
 .code
 SobelAsm proc	;	byte[] rgbValues,	byte[] grayValues,	int width,		int height,		int scanWidth,				int detectionLevel
 				;	rcx,				rdx,				r8,				r9,				qword ptr [rsp+8*5],		qword ptr [rsp+8*6]
@@ -18,6 +21,11 @@ push r15
 push rcx ; save rgbValues on stack for later use
 push rdx ; save grayValues on stack for later use
 ; stack moved by 8*7
+
+; initialize SSE registers
+vmovdqa xmm5, xmmword ptr [ShuffleRGB]
+vmovdqa xmm4, xmmword ptr [MaskRGB]
+vmovdqa xmm3, xmmword ptr [DivideByThree]
 
 mov r12, rcx ; r12 = rgbValues
 mov r13, rdx ; r13 = grayValues
@@ -34,6 +42,35 @@ sub r11, rax ; r11 = scanWidth - width*3 = widthDifference
 mov r14, r9 ; set outer loop counter to height
 l1:
 	mov r15, r8 ; set inner loop counter to width
+	l1_0:
+		cmp r15, 16
+		jl l1_0_end
+		vmovdqu xmm0, xmmword ptr [r12]
+		vpshufb xmm0, xmm0, xmm5
+		vpmaddubsw xmm0, xmm0, xmm4
+		vphaddw xmm0, xmm0, xmm5
+		vpmovzxwd xmm0, xmm0
+		vcvtdq2ps xmm0, xmm0
+
+		vdivps xmm0, xmm0, xmm3
+
+		vcvttps2dq xmm0, xmm0
+
+		vpackusdw xmm0, xmm0, xmm0
+		vpackuswb xmm0, xmm0, xmm0
+
+		vmovd dword ptr [r13], xmm0
+
+		mov qword ptr [r12], 0000000000000000h
+		mov dword ptr [r12+8], 00000000h
+
+		add r12, 12
+		add r13, 4
+		sub r15, 4
+		jmp l1_0
+	l1_0_end:
+		cmp r15, 0
+		je l1_1_end
 	l1_1:
 		movzx eax, byte ptr [r12]
 		movzx ecx, byte ptr [r12+1]
@@ -53,6 +90,7 @@ l1:
 		inc r13
 		dec r15
 		jnz l1_1
+	l1_1_end:
 	add r12, r11
 	dec r14
 	jnz l1
@@ -60,8 +98,7 @@ l1:
 ; initialize SSE registers
 vpxor xmm5, xmm5, xmm15
 vmovdqa xmm4, xmmword ptr [ShuffleMatrixA]
-vmovdqa xmm3, xmmword ptr [MatrixX]
-vmovdqa xmm2, xmmword ptr [MatrixY]
+vmovdqa xmm3, xmmword ptr [MatrixXandY]
 
 mov r10, r8 ; r10 = width
 
@@ -89,22 +126,15 @@ l2:
 		vpinsrd xmm0, xmm0, dword ptr [r13+2*r10-1], 2
 		vpshufb xmm0, xmm0, xmm4
 
-		vpmaddubsw xmm1, xmm0, xmm2 ; xmm1 = MatrixA * MatrixY => xmm1 = y0+y1, y2+y3, y4+y5, y6+y7 => byte to word
-		vpmovsxwd xmm1, xmm1 ; word to double word
-		vphaddd xmm1, xmm1, xmm5 ; (y0+y1) + (y2+y3), (y4+y5) + (y6+y7)
-		vphaddd xmm1, xmm1, xmm5 ; Gy = ((y0+y1) + (y2+y3)) + ((y4+y5) + (y6+y7))
-		vpmuldq xmm1, xmm1, xmm1 ; Gy * Gy
-
-		vpmaddubsw xmm0, xmm0, xmm3 ; xmm0 = MatrixA * MatrixX => xmm0 = x0+x1, x2+x3, x4+x5, x6+x7 => byte to word
+		vpmaddubsw xmm0, xmm0, xmm3 ; xmm1 = MatrixA * MatrixY => xmm1 = y0+y1, y2+y3, y4+y5, y6+y7 => byte to word
+		vphaddw xmm0, xmm0, xmm5 ; (y0+y1) + (y2+y3), (y4+y5) + (y6+y7)
 		vpmovsxwd xmm0, xmm0 ; word to double word
-		vphaddd xmm0, xmm0, xmm5 ; (x0+x1) + (x2+x3), (x4+x5) + (x6+x7)
-		vphaddd xmm0, xmm0, xmm5 ; Gx = ((x0+x1) + (x2+x3)) + ((x4+x5) + (x6+x7))
-		vpmuldq xmm0, xmm0, xmm0 ; Gx * Gx
-
-		vpaddq xmm0, xmm0, xmm1 ; G^2 = Gx^2 + Gy^2
+		vphaddd xmm0, xmm0, xmm5 ; Gy = ((y0+y1) + (y2+y3)) + ((y4+y5) + (y6+y7))
+		vpmulld xmm0, xmm0, xmm0 ; Gy * Gy
+		vphaddd xmm0, xmm0, xmm5 ; G^2 = Gx^2 + Gy^2
 
 		vmovq rax, xmm0
-		cmp rax, rcx ; compare G^2 with detectionLevel
+		cmp eax, ecx ; compare G^2 with detectionLevel
 		jl end_edge
 
 		mov word ptr [r12], 0FFFFh
